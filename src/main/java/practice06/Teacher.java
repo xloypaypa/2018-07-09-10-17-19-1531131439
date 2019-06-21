31b31b597ec0912e6df3f535110a6a0ff6011f14
xsu <xsu@thoughtworks.com>
2019-06-21 16:15:17 +0800

    add teacher class.

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,29 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person {
+
+    private final Integer klass;
+
+    public Teacher(String name, int age) {
+        super(name, age);
+        this.klass = null;
+    }
+
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Integer getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String introduce() {
+        if (this.klass != null) {
+            return super.introduce() + " I am a Teacher. I teach Class " + this.klass + ".";
+        } else {
+            return super.introduce() + " I am a Teacher. I teach No Class.";
+        }
+    }
 }
