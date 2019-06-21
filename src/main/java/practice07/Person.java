3b90ca3c923ee916d1339c035468bab09e0983d5
xsu <xsu@thoughtworks.com>
2019-06-21 16:16:02 +0800

    copy Person class.

--- src/main/java/practice07/Person.java
+++ src/main/java/practice07/Person.java
@@ -1,4 +1,25 @@
 package practice07;
 
 public class Person {
+
+    private final String name;
+    private final int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String introduce() {
+        return "My name is " + this.getName() + ". I am " + this.age + " years old.";
+    }
+
 }
