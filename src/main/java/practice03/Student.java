df5c536cc8dc4e54aed286e2edd22a733652320e
xsu <xsu@thoughtworks.com>
2019-06-21 16:03:06 +0800

    copy practice 2

--- src/main/java/practice03/Student.java
+++ src/main/java/practice03/Student.java
@@ -1,4 +1,22 @@
 package practice03;
 
-public class Student {
+import practice02.Person;
+
+public class Student extends Person {
+
+    private final int klass;
+
+    public Student(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String introduce() {
+        return "I am a Student. I am at Class " + this.klass + ".";
+    }
 }
