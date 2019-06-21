c0390f28025803b5868bcf5a0605e84ec90a73e5
xsu <xsu@thoughtworks.com>
2019-06-21 16:27:46 +0800

    refactor klass

--- src/main/java/practice07/Teacher.java
+++ src/main/java/practice07/Teacher.java
@@ -1,4 +1,33 @@
 package practice07;
 
-public class Teacher {
+public class Teacher extends Person {
+
+    private final Klass klass;
+
+    public Teacher(String name, int age) {
+        super(name, age);
+        this.klass = Klass.noClass;
+    }
+
+    public Teacher(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String introduce() {
+        return super.introduce() + " I am a Teacher. I teach " + this.klass.getDisplayName() + ".";
+    }
+
+    public String introduceWith(Student student) {
+        if (student.getKlass().equals(this.klass)) {
+            return super.introduce() + " I am a Teacher. I teach " + student.getName() + ".";
+        } else {
+            return super.introduce() + " I am a Teacher. I don't teach " + student.getName() + ".";
+        }
+    }
 }
